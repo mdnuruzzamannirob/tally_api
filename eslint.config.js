c1319ef9fa941466789fe16976bbdb38029024b1
Md Nuruzzaman <dev.mdnuruzzaman@gmail.com>
2026-09-04 16:41:06 +0600

    Update .gitignore

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import eslint from "@eslint/js";
 import prettier from "eslint-config-prettier";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-
 export default tseslint.config(
   {
     ignores: ["dist/**", "src/generated/**", "coverage/**"],
